638862d2fa0dfc219de76a5a583e19a92f27cad1
marcushanok <hanokmarcusmaggidi@gmail.com>
2023-03-03 18:08:00 +0530

    kk

--- app.js
+++ app.js
@@ -1,5 +1,52 @@
-const _ = require('lodash');
+const {readFile, writeFile} = require('fs');
 
-const items = [1, [2, [3,[4]]]]
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+
+const getText = (path) =>{
+    return new Promise((resolve,reject)=>{
+        readFile(path, 'utf8', (err,data)=>{
+            if(err){
+                reject(err)
+            }
+            else{
+                resolve(data)
+            }
+        })
+    })
+}
+
+// getText('./content/first.text').then((result) => console.log(result)).catch((err) => console.log(err))
+
+const start = async() =>{
+    try{
+        const first = await getText('./content/first.txt');
+        const second = await getText('./content/second.txt');
+        console.log(first, second);
+    }
+    catch(error){
+        console.log(error)
+    }
+    
+}
+
+// start()
+
+
+const util = require('util');
+const readFilePromise = util.promisify(readFile);
+const writeFilePromisse = util.promisify(writeFile);
+
+const start1 = async () =>{
+    try{
+        const first = await readFilePromise('.content/first.txt', 'utf8');
+        const second = await readFilePromise('./content/second.txt', 'utf8');
+        await writeFilePromisse('./content/result-mind-grenade.txt', `This is just AWESOME : ${first} ${second}`);
+        console.log(first, second);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+// start1()
+
+// const {readFile, writeFile} = require('fs'),promises
